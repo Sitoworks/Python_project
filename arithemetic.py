41c948669514682f4eca376f80cebfcc1d4a0b75
sitoworks <sitoworks@hotmail.com>
2025-08-13 11:51:06 +0100

    Add implementation to calculate the area of a circle

---
+++ arithemetic.py
@@ -0,0 +1,16 @@
+import math
+
+# radius = float(input("what is the radius of the circle: "))
+# circumference = 2 * math.pi * radius
+
+# print(f"The circumference is: {circumference} ")
+
+
+
+# Calculating the area of a circle
+radius = float(input("what is the radius of the circle: "))
+# area of a circle is pi.r2
+
+area = 2 * math.pi * pow(radius, 2)
+
+print(f"The area of the circle is: {round(area, 3)}cm ")   
