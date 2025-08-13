5b27462d82de536f31e8bdd00e7708f01269bd41
sitoworks <sitoworks@hotmail.com>
2025-08-13 11:55:35 +0100

    Add age verification logic to shopping cart script

---
+++ shoppingcart.py
@@ -0,0 +1,15 @@
+# Example: Using if statement in a shopping cart
+
+#item_price = 50
+# budget = 40
+
+# if item_price <= budget:
+  #  print("You can buy this item!")
+# else:
+#    print("This item is too expensive.")
+
+age = int(input("Enter your age: "))
+if age >= 16:
+    print("You are now signed up!")
+else:
+    print("You must be 16+ to be able to sign up!")
