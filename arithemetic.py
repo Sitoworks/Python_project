0c4fd378d8ecb13a992d832a182aadeaa368b6a7
sitoworks <sitoworks@hotmail.com>
2025-08-13 12:18:01 +0100

    add white line spacing to arithemetic file

--- arithemetic.py
+++ arithemetic.py
@@ -9,6 +9,7 @@ import math
 radius = float(input("what is the radius of the circle: "))
 # area of a circle is pi.r2
 
+
 area = 2 * math.pi * pow(radius, 2)
 
 print(f"The area of the circle is: {round(area, 3)}cm ")   
