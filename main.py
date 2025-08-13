c02483df7e9e64ca2eb5fdce7dc50430f28b4549
sitoworks <sitoworks@hotmail.com>
2025-08-13 11:47:31 +0100

    Add initial implementation to calculate the area of a triangle

---
+++ main.py
@@ -0,0 +1,6 @@
+# Calc the area of a Trianagle
+lenght = float(input("input the lenghtS: "))
+width = float(input("input the width: "))
+area = lenght * width
+
+print(f"the area of the object is {area}cm")
